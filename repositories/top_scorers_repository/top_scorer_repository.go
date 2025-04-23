5b5d841af4db5f2be5b41773d7907b63cffaa81e
Nahuel Ojea <nojea@externos.idealista.com>
2025-04-23 17:28:12 +0200

    Fix top scorers

--- repositories/top_scorers_repository/top_scorer_repository.go
+++ repositories/top_scorers_repository/top_scorer_repository.go
@@ -3,6 +3,7 @@ package top_scorers_repository
 import (
 	"context"
 	"math"
+	"time"
 
 	"github.com/nahuelojea/handballscore/config/db"
 	"github.com/nahuelojea/handballscore/models"
@@ -19,132 +20,123 @@ type GetTopScorersOptions struct {
 }
 
 func GetTopScorers(filterOptions GetTopScorersOptions) ([]models.TopScorer, int64, int, error) {
-	ctx := context.TODO()
-	database := db.MongoClient.Database(db.DatabaseName)
-	collection := database.Collection("match_players_view")
+	// 1. Context con timeout para evitar bloqueos
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
-	pipeline := []bson.M{
-		{
-			"$match": bson.M{
-				"association_id": filterOptions.AssociationId,
-				"goals.total":    bson.M{"$gt": 0},
-			},
-		},
-		{
-			"$lookup": bson.M{
-				"from": "matches",
-				"let":  bson.M{"match_id": "$match_id"},
-				"pipeline": []bson.M{
-					{
-						"$match": bson.M{
-							"$expr": bson.M{
-								"$and": []bson.M{
-									{"$eq": []interface{}{"$_id", bson.M{"$toObjectId": "$$match_id"}}},
-									{"$eq": []interface{}{"$tournament_category_id", filterOptions.TournamentCategoryId}},
-									{"$not": bson.M{"$in": []interface{}{"$status", []string{models.Created, models.Programmed}}}},
-								},
-							},
-						},
-					},
-					{"$limit": 1},
-				},
-				"as": "match_info",
-			},
-		},
-		{
-			"$unwind": bson.M{
-				"path":                       "$match_info",
-				"preserveNullAndEmptyArrays": false,
-			},
-		},
-		{
-			"$group": bson.M{
-				"_id":            "$player_id",
-				"total_goals":    bson.M{"$sum": "$goals.total"},
-				"total_matches":  bson.M{"$addToSet": "$match_id"},
-				"player_name":    bson.M{"$first": "$player_name"},
-				"player_surname": bson.M{"$first": "$player_surname"},
-				"player_avatar":  bson.M{"$first": "$player_avatar"},
-				"team_name":      bson.M{"$first": "$team_name"},
-				"team_avatar":    bson.M{"$first": "$team_avatar"},
-			},
-		},
-	}
+	coll := db.MongoClient.Database(db.DatabaseName).Collection("match_players_view")
 
+	// Pre-cálculo de skip y limit
+	skip := int64((filterOptions.Page - 1) * filterOptions.PageSize)
+	limit := int64(filterOptions.PageSize)
+
+	// 2. Pipeline optimizado
+	pipeline := make([]bson.M, 0, 8)
+
+	// 2.1. Filtrar por asociación y goles > 0 (usa índices)
+	pipeline = append(pipeline, bson.M{"$match": bson.M{
+		"association_id": filterOptions.AssociationId,
+		"goals.total":    bson.M{"$gt": 0},
+	}})
+
+	// 2.2. Lookup solo de partidos relevantes (match_id y categoría)
+	pipeline = append(pipeline, bson.M{"$lookup": bson.M{
+		"from": "matches",
+		"let":  bson.M{"mid": "$match_id"},
+		"pipeline": []bson.M{{
+			"$match": bson.M{"$expr": bson.M{"$and": []bson.M{
+				{"$eq": []interface{}{"$_id", bson.M{"$toObjectId": "$$mid"}}},
+				{"$eq": []interface{}{"$tournament_category_id", filterOptions.TournamentCategoryId}},
+				{"$not": bson.M{"$in": []interface{}{"$status", []string{models.Created, models.Programmed}}}},
+			}}},
+		}},
+		"as": "match_info",
+	}})
+
+	// 2.3. Desenrollar y descartar no match
+	pipeline = append(pipeline, bson.M{"$unwind": bson.M{
+		"path":                       "$match_info",
+		"preserveNullAndEmptyArrays": false,
+	}})
+
+	// 2.4. Agrupar goles y partidos por jugador
+	pipeline = append(pipeline, bson.M{"$group": bson.M{
+		"_id":            "$player_id",
+		"total_goals":    bson.M{"$sum": "$goals.total"},
+		"total_matches":  bson.M{"$addToSet": "$match_id"},
+		"player_name":    bson.M{"$first": "$player_name"},
+		"player_surname": bson.M{"$first": "$player_surname"},
+		"player_avatar":  bson.M{"$first": "$player_avatar"},
+		"team_name":      bson.M{"$first": "$team_name"},
+		"team_avatar":    bson.M{"$first": "$team_avatar"},
+	}})
+
+	// 2.5. Filtrado opcional por nombre/apellido
 	if filterOptions.Name != "" {
-		pipeline = append(pipeline, bson.M{
-			"$match": bson.M{
-				"$or": []bson.M{
-					{"player_name": bson.M{"$regex": filterOptions.Name, "$options": "i"}},
-					{"player_surname": bson.M{"$regex": filterOptions.Name, "$options": "i"}},
-				},
-			},
-		})
+		pipeline = append(pipeline, bson.M{"$match": bson.M{"$or": []bson.M{
+			{"player_name": bson.M{"$regex": filterOptions.Name, "$options": "i"}},
+			{"player_surname": bson.M{"$regex": filterOptions.Name, "$options": "i"}},
+		}}})
 	}
 
-	facetPipeline := []bson.M{
-		{
-			"$addFields": bson.M{
-				"total_matches": bson.M{"$size": "$total_matches"},
-				"average": bson.M{
-					"$cond": []interface{}{
-						bson.M{"$eq": []interface{}{bson.M{"$size": "$total_matches"}, 0}},
-						0,
-						bson.M{"$divide": []interface{}{"$total_goals", bson.M{"$size": "$total_matches"}}},
-					},
-				},
-			},
-		},
-		{
-			"$sort": bson.M{
-				"total_goals": -1,
-				"_id":         1,
-			},
-		},
-		{
-			"$facet": bson.M{
-				"data": []bson.M{
-					{"$skip": (filterOptions.Page - 1) * filterOptions.PageSize},
-					{"$limit": filterOptions.PageSize},
-				},
-				"total": []bson.M{
-					{"$count": "totalRecords"},
-				},
-			},
-		},
-		{
-			"$unwind": "$total",
-		},
-	}
+	// 2.6. Calcular total_matches y average
+	pipeline = append(pipeline, bson.M{"$addFields": bson.M{
+		"total_matches": bson.M{"$size": "$total_matches"},
+		"average": bson.M{"$cond": []interface{}{
+			bson.M{"$eq": []interface{}{bson.M{"$size": "$total_matches"}, 0}},
+			0,
+			bson.M{"$divide": []interface{}{"$total_goals", bson.M{"$size": "$total_matches"}}},
+		}},
+	}})
 
-	pipeline = append(pipeline, facetPipeline...)
+	// 2.7. Facet para datos y recuento en un solo pipeline
+	pipeline = append(pipeline, bson.M{"$facet": bson.M{
+		"data": []bson.M{
+			{"$sort": bson.M{"total_goals": -1, "_id": 1}},
+			{"$skip": skip},
+			{"$limit": limit},
+		},
+		"total": []bson.M{
+			{"$count": "totalRecords"},
+		},
+	}})
 
-	opts := options.Aggregate().SetAllowDiskUse(true)
-	cur, err := collection.Aggregate(ctx, pipeline, opts)
+	// 2.8. Desenrollar total y proyectar resultado final
+	pipeline = append(pipeline,
+		bson.M{"$unwind": "$total"},
+		bson.M{"$project": bson.M{
+			"data":         1,
+			"totalRecords": "$total.totalRecords",
+		}},
+	)
+
+	// 3. Ejecutar agregación con allowDiskUse y batchSize
+	opts := options.Aggregate().
+		SetAllowDiskUse(true).
+		SetBatchSize(100)
+
+	cur, err := coll.Aggregate(ctx, pipeline, opts)
 	if err != nil {
 		return nil, 0, 0, err
 	}
 	defer cur.Close(ctx)
 
-	var result struct {
-		Data  []models.TopScorer `bson:"data"`
-		Total struct {
-			TotalRecords int64 `bson:"totalRecords"`
-		} `bson:"total"`
+	// 4. Leer resultados
+	var res struct {
+		Data         []models.TopScorer `bson:"data"`
+		TotalRecords int64              `bson:"totalRecords"`
 	}
-
 	if cur.Next(ctx) {
-		if err := cur.Decode(&result); err != nil {
+		if err := cur.Decode(&res); err != nil {
 			return nil, 0, 0, err
 		}
 	}
-
-	totalRecords := int64(0)
-	if len(result.Data) > 0 {
-		totalRecords = result.Total.TotalRecords
+	if err := cur.Err(); err != nil {
+		return nil, 0, 0, err
 	}
 
-	totalPages := int(math.Ceil(float64(totalRecords) / float64(filterOptions.PageSize)))
+	// 5. Calcular totalPages
+	totalPages := int(math.Ceil(float64(res.TotalRecords) / float64(filterOptions.PageSize)))
 
-	return result.Data, totalRecords, totalPages, nil
+	return res.Data, res.TotalRecords, totalPages, nil
 }
